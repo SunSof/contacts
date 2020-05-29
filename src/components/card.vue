22ca05b9c9654a21fd72fca1447c4eaaaf8713d7
SunSof <buketovasofi@gmail.com>
2020-05-29 15:56:25 +0300

    add change card

--- src/components/card.vue
+++ src/components/card.vue
@@ -1,11 +1,39 @@
 <template>
   <div class="b-card">
-    <div class="text-center">
+    <div v-if="isVisible" class="text-center">
       <h2 class="text-lg">{{ name }}</h2>
       <div class="text">{{ email }}</div>
       <div class="text">{{ phone }}</div>
+    </div>
+    <div v-else class="container b-card__input">
+      <input
+        type="text"
+        name="title"
+        id="title"
+        class="form-control"
+        v-model="name"
+        placeholder=""
+      />
+      <input
+        type="text"
+        name="body"
+        class="form-control"
+        v-model="email"
+        placeholder=""
+      />
+      <input
+        type="text"
+        name="body"
+        class="form-control"
+        v-model="phone"
+        placeholder=""
+      />
+    </div>
+    <div class="card-btn">
       <button class="deleted-button btn" @click="remove">delete</button>
-      <button class="change-button btn">change</button>
+      <button class="change-button btn" @click="isVisible = !isVisible">
+        change
+      </button>
     </div>
   </div>
 </template>
@@ -15,12 +43,15 @@ export default {
   name: 'card-list',
   props: ['id', 'name', 'email', 'phone'],
   data () {
-    return {}
+    return {
+      isVisible: true
+    }
   },
   methods: {
     remove () {
       this.$emit('deleteContact', this.id)
-    }
+    },
+    changeCard () {}
   }
 }
 </script>
@@ -28,9 +59,17 @@ export default {
 <style>
 .b-card {
   background-color: rgb(233, 240, 241);
-  padding: 20px;
+  padding: 40px 20px;
 }
 .text {
+  font-family: "Raleway", sans-serif;
+  font-size: 15px;
+}
+.card-btn {
+  text-align: center;
+}
+.form-control {
+  background: rgba(0, 0, 0, 0);
   font-family: "Raleway", sans-serif;
   font-size: 15px;
 }
